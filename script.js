3f4be1e7dfcd29e218b974161d39c83ca1414f72
TanbirBora <149858955+TanbirBora@users.noreply.github.com>
2026-01-31 13:13:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const SHEET_ID = "1CGPkZMvXf7v4H1f0AdigX6oM6qxN5RWjz5-mmhGCmYY";
 const SHEET_NAME = "Creator";
 
-const url = `https://docs.google.com/spreadsheets/d/${1CGPkZMvXf7v4H1f0AdigX6oM6qxN5RWjz5-mmhGCmYY}/gviz/tq?tqx=out:json&sheet=${Creator}`;
+const url = `https://docs.google.com/spreadsheets/d/1Y1KvTCzKQ-san-MiXFQofVResbzGDBNFTQRZ0tmqsDI/gviz/tq?tqx=out:json&sheet=Jio Comparision Plans
+`;
 
 document.getElementById("loadBtn").onclick = () => {
   fetch(url)
